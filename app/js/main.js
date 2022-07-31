a264a6e28f6a6571a9c80abf6d1fc108b2c72cf0
AnnDS99 <biskupag8@gmail.com>
2022-07-31 10:04:54 +0200

    addea product

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,16 @@
 $(function () {
-  
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     fade: true
   })
+  $('.star').rateYo({
+    // rating: 3.6,
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  })
+
 });
